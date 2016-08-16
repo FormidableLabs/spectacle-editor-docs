79c1eaa5d20ffb64aef4dae03a01e2174d195bd4
Paula Lavalle <lavalle.paula@gmail.com>
2016-08-16 15:54:26 -0700

    Add ReactGA to client render entry point

--- src/components/app.js
+++ src/components/app.js
@@ -1,16 +1,10 @@
 import React from "react";
 import Radium, { Style, StyleRoot } from "radium";
-import ReactGA from "react-ga";
 
 // base stylesheet
 import stylesheet from "../spectacle-editor-stylesheet";
 
 class App extends React.Component {
-  componentDidMount() {
-    ReactGA.initialize("UA-43290258-1");
-    ReactGA.set({ "page": "/open-source/spectacle-editor" });
-  }
-
   render() {
     /*eslint-disable no-undef */
     const isBrowser = typeof window !== "undefined" && window.__STATIC_GENERATOR !== true;
