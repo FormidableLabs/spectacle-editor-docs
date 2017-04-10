a26e6a96879529d831ce9c25aef28a0600448a1b
David Von Lehman <david.vonlehman@gmail.com>
2017-04-10 12:44:04 -0700

    Update download links to v0.1.4 release
    https://github.com/FormidableLabs/spectacle-editor/releases/tag/v0.1.4

--- src/screens/home/components/download.js
+++ src/screens/home/components/download.js
@@ -99,7 +99,7 @@ class Download extends React.Component {
             </p>
             <a
               key="downloadMac"
-              href="https://github.com/FormidableLabs/spectacle-editor/releases/download/v0.1.3/Spectacle.Editor-0.1.3.dmg"
+              href="https://github.com/FormidableLabs/spectacle-editor/releases/download/v0.1.4/Spectacle.Editor-0.1.4.dmg"
               style={styles.button}
             >
               <p style={styles.buttonText}>
@@ -123,7 +123,7 @@ class Download extends React.Component {
             <a
               key="downloadWin"
               style={styles.button}
-              href="https://github.com/FormidableLabs/spectacle-editor/releases/download/v0.1.3/Spectacle.Editor.Setup.0.1.3.exe"
+              href="https://github.com/FormidableLabs/spectacle-editor/releases/download/v0.1.4/Spectacle.Editor.Setup.0.1.4.exe"
             >
               <p style={styles.buttonText}>
                 Download
